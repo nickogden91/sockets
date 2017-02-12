6714b1093559d5f47e11f364262578598c6dd360
nickogden91 <nick@kunasystems.com>
2017-02-12 11:55:19 -0800

    send/reveive file seems to work

--- sockettest.py
+++ sockettest.py
@@ -16,33 +16,71 @@ class SocketTest:
 class SocketTestClient:
 
     def __init__(self):
-        s = socket.socket()
-        s.connect(("localhost",2424))
-        f = open ("test.jpg", "rb")
+        self.s = socket.socket()
+        self.s.connect(("localhost",2424))
+        self.get_file('test.jpg')
+        
+    def send_file(self, fn):
+        self.s.send("SEND FILE")
+        f = open(fn, "rb")
         l = f.read(1024)
-        while (l):
-            s.send(l)
+        while l:
+            self.s.send(l)
             l = f.read(8096)
-        s.close()
+        f.close()
+        self.s.close()
+        
+    def get_file(self, fn):
+        self.s.send("GET FILE")
+        f = open(fn, "wb")
+        l = self.s.recv(8096)
+        while l:
+            f.write(l)
+            l = self.s.recv(8096)
+        f.close()
+        self.s.close()
     
 class SocketTestServer:
     
     def __init__(self):
-        s = socket.socket()
-        s.bind(("localhost",2424))
-        s.listen(5)
+        self.s = socket.socket()
+        self.s.bind(("localhost",2424))
+        self.s.listen(5)
+        self.wait()
+        
+    def wait(self):
         while True:
-            sc, address = s.accept()
-            print address
-            f = open('images/test.jpg','wb') #open in binary
-            while (True):       
-                l = sc.recv(8096)
-                while (l):
-                        f.write(l)
-                        l = sc.recv(8096)
-            f.close()
-            sc.close()
-        s.close()
+            self.sc, self.address = self.s.accept()
+            command = self.sc.recv(8096)
+            print command
+            if command == 'SEND FILE':
+                self.receive_file('images/test.jpg')
+            elif command == 'GET FILE':
+                self.send_file('images/test.jpg')
+            else:
+                print 'Unrecognized Command'
+        self.s.close()
+        
+    def receive_file(self, fn):
+        f = open(fn,'wb')
+        while (True):       
+            l = self.sc.recv(8096)
+            while (l):
+                    f.write(l)
+                    l = self.sc.recv(8096)
+        f.close()
+        self.sc.close()
+        
+    def send_file(self, fn):
+        f = open(fn,'rb')
+        while (True):    
+            l = f.read(8096)
+            while (l):
+                self.sc.send(l)
+                l = f.read(8096)
+        f.close()
+        self.sc.close()
+        
     
 if __name__ == '__main__':
     if len(sys.argv) == 1:
